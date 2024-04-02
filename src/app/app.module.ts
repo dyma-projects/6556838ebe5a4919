8e726a78949a5a772fbe200d2ff247bb0a49b5c6
saad kammous <saadkammouss@gamil.com>
2024-04-02 14:03:19 +0000

    2eme projet

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,21 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { Exercice2Component } from './exercice2/exercice2.component';
+import { Exercice1Component } from './exercice1/exercice1.component';
+import { Exercice1EnfantComponent } from './exercice1/exercice1-enfant/exercice1-enfant.component';
+import { Exercice3Component } from './exercice3/exercice3.component';
+import { Exercice3EnfantComponent } from './exercice3/exercice3-enfant/exercice3-enfant.component';
+import { Exercice4Component } from './exercice4/exercice4.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    Exercice2Component
+    Exercice2Component,
+    Exercice1Component,
+    Exercice1EnfantComponent,
+    Exercice3Component,
+    Exercice3EnfantComponent,
+    Exercice4Component
   ],
   imports: [
     BrowserModule
